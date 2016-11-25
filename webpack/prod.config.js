f1b37efc05e34554aefe8911f7d5bf882248e95d
Abdul Khan <invalidred@gmail.com>
2016-11-24 22:07:17 -0500

    updated project with proper assets

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -5,7 +5,7 @@ const prodConfig = Object.assign({}, baseConfig, {
   output: {
     path: 'dist/',
     filename: 'app.bundle.js',
-    publicPath: '/focus-noise'
+    publicPath: '/focus-noise/'
   },
   devtool: 'source-map',
   plugins: baseConfig.plugins.concat([
